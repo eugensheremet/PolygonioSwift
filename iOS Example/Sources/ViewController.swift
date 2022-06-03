57e4e4d551b830aa256d80051f8d99cf922a89cd
Eugene Sheremet <eugene@unest.co>
2022-06-03 17:17:40 +0300

    Candle volume to Decimal value

--- iOS Example/Sources/ViewController.swift
+++ iOS Example/Sources/ViewController.swift
@@ -9,108 +9,21 @@ import UIKit
 import PolygonioSwift
 
 class ViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        let polygon = PolygonioSwift.Client(key: "YOUR_API_KEY")
-        
-        polygon.setDebug(enable: true)
-        
-        // Market Status
-        polygon.marketStatus { (result: MarketStatusResponse?, err) in
-            // check if we got any errors
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-                print(result?.market)
-            }
-        }
-        
-        // Ticker News
-        polygon.tickerNews(ticker: "AAPL",limit: 5, order: .ascending, publishedLessEqualThan: nil, publishedGreaterEqualThan: nil) { (result:TickerNewsResponse?, err) in
-            // check if we got any errors
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-            }
-        }
-        
-        // Market Holidays
-       polygon.marketHolidays { (result: [MarketHolidaysResponse?], err) in
-            // check if we got any errors
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-            }
-        }
-        
-        // Daily Open Close
-        polygon.dailyOpenClose(symbol: "AAPL", date: "2020-09-11") { (result: DailyOpenCloseResponse?, err) in
-            // check if we got any errors
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-            }
-        }
-        
-        // Tickers Query
-        polygon.tickers(sort: .ticker, order: nil, type: nil, market: .Stocks, locale: nil, ticker: "AAPL", limit: 100, active: true) { (result:TickersQueryResponse?, err) in
-            // check if we got any errors
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-                print(result?.results)
-            }
-        }
-        
-        // Ticker Search
-        polygon.searchTickers(search: "Vis", type: .CommonStock) {
-            (result:TickerSearchResponse?, err) in
-            // check if we got any errors
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-                print(result?.results)
-            }
-        }
-        
-        // Stock Financials
-        polygon.stockFinancials(symbol: "AAPL", limit: 5, type: .Year) { (result: StockFinancialsResponse?, err) in
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-            }
-        }
-        
-        // Ticker Details
-        polygon.tickerDetails(symbol: "AAPL") { (result:TickerDetailsResponse?, err) in
-            // check if we got any errors
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-            }
-        }
-        
-        // Fetch Aggregates (Candle Data)
-        polygon.aggregates(ticker: "AAPL", multiplier: 1, timespan: .day, from: "2020-06-01", to: "2020-12-01") { (result:AggregatesResponse?, err) in
-            // check if we got any errors
-            if let err = err {
-                print(err)
-            } else {
-                print(result)
-                print(result?.results)
-            }
-        }
-    }
-}
 
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		let polygon = PolygonioSwift.Client(key: "l5PkSoz69UG1UT_x1vuugm0EmJJdzReF")
+		polygon.setDebug(enable: true)
+
+		polygon.aggregates(ticker: "X:BTCUSD", multiplier: 1, timespan: .day, from: "2021-07-22", to: "2021-08-22") { (result:AggregatesResponse?, err) in
+			// check if we got any errors
+			if let err = err {
+				print(err)
+			} else {
+				print(result)
+				print(result?.results)
+			}
+		}
+	}
+}
